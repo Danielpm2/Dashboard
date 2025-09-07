0ccc90111a245652fbe1200af3c9f7bfa1a53f04
Danielpm2 <danielsalinas0077@gmail.com>
2025-09-07 19:45:11 -0400

    NEW START OF A FRONTEND (fully made by AI but i will do the rest)

--- frontend-legacy/server.js
+++ frontend-legacy/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3067;
+const PORT = 3066;
 
 // Serve static files (CSS, JS, images, etc.)
 app.use(express.static(__dirname));
